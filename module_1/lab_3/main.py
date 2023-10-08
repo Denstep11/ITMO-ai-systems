72e44aff3293f5b1ef54e6fcf3b50548b1579aeb
Denstep <denvvbaranov@gmail.com>
2023-10-09 00:05:21 +0300

    update lab_3

--- module_1/lab_3/main.py
+++ module_1/lab_3/main.py
@@ -7,60 +7,58 @@ command_list = [
         'help': 'Запрос: «Кто дружит с [Игрок]?»',
         'query': "friend('{0}',X)",
         'vars': ('X'),
-        'answer': lambda players: f'C ним/ней дружит {", а еще ".join(players)}.'
+        'answer': lambda players: 'Игкрок дружит с '+", ".join(players) + '.'
     },
 
     {
         'regex': r'^(.+) дружит с (.+)\?$',
         'help': 'Запрос: «[Игрок А] дружит с [Игрок Б]?»',
-        'query': "nickname('{0}'), nickname('{1}'),friend('{0}','{1}')",
+        'query': "friend('{0}','{1}')",
         'vars': None,
-        'answer': lambda res: "Да, это так." if res else "Нет, это не так."
+        'answer': lambda res: "Да!" if res else "Нет:("
     },
 
     {
         'regex': r'^(.+) может играть с (.+)\?$',
         'help': 'Запрос: «[Игрок А] может играть с [Игрок Б]?»',
-        'query': "nickname('{0}'),nickname('{1}'),can_play_together('{0}','{1}')",
+        'query': "can_play_together('{0}','{1}')",
         'vars': None,
-        'answer': lambda res: "Да, это так." if res else "Нет, это не так."
+        'answer': lambda res: "Да!" if res else "Нет:("
     },
 
     {
         'regex': r'^Какой ранг у (.+)\?$',
         'help': 'Запрос: «Какой ранг у [Игрок]?»',
-        'query': "nickname('{0}'),player_rank('{0}',X)",
+        'query': "player_rank('{0}',X)",
         'vars': ('X'),
-        'answer': lambda level:
-            'Непонятно какой ранг.' if not level
-            else f'Он(а) достиг(ла) {level} ранга.'
+        'answer': lambda rank: 'Непонятно какой ранг.' if not rank else 'Игрок достиг ' + "".join(rank) + ' ранга.'
     },
     {
         'regex': r'^С кем может играть игрок (.+)\?$',
         'help': 'Запрос: «С кем может играть игрок [Игрок]?»',
-        'query': r"nickname('{0}'),can_play_with('{0}',X), X \= '{0}'",
+        'query': r"can_play_with('{0}',X), X \= '{0}'",
         'vars': ('X'),
         'answer': lambda players:
-            'Нет игроков с таким же рангом.' if len(players) == 0
-            else f'У следующих игроков такой же ранга как и у него/нее: { ", ".join(players) }.'
+            'Нет игроков с таким же рангом, игроку не с кем играть(' if not players
+            else 'Игрок может играсть с этими игроками: ' + ", ".join(players) + '.'
     },
     {
         'regex': r'^Кто потенциальный друг (.+)\?$',
         'help': 'Запрос: «Кто потенциальный друг [Игрок]?»',
-        'query': r"nickname('{0}'),potential_friends('{0}',X)",
+        'query': r"potential_friends('{0}',X)",
         'vars': ('X'),
         'answer': lambda players:
-            'Нет потенциальных друзей.' if len(players) == 0
-            else f'Список потенциальных друзией: { ", ".join(players) }.'
+            'Нет потенциальных друзей.' if not players
+            else 'Список потенциальных друзией: ' + ", ".join(players) + '.'
     },
     {
         'regex': r'^Какие реальные имена друзей игрока (.+)\?$',
         'help': 'Запрос: «Какие реальные имена друзей игрока [Игрок]?»',
-        'query': r"nickname('{0}'),friends_real_name('{0}',X)",
+        'query': r"friends_real_name('{0}',X)",
         'vars': ('X'),
         'answer': lambda players:
             'Нет друзей.' if len(players) == 0
-            else f'Список реальных имен друзией: { ", ".join(players) }.'
+            else f'Список реальных имен друзией: ' + ", ".join(players) + '.'
     },
 ]
 def help_messege():
@@ -87,6 +85,10 @@ def read_request():
         if s == "help":
             help_messege()
             continue
+
+        if s == "":
+            continue
+
         yield s
 
 def parse_request(req: str):
@@ -95,8 +97,8 @@ def parse_request(req: str):
     for i in command_list:
         match = re.search(i['regex'], req, re.IGNORECASE)
         if match:
-            print(match.groups())
             return i, match
+
     print("Проверьте корректность запроса!")
     return None
 
@@ -113,6 +115,7 @@ def query_in_kb(format, values, prolog: Prolog):
             result.append(sol[format['vars']])
         else:
             result.append(sol)
+
     return result
 
 def print_result(format, res):
@@ -130,5 +133,6 @@ def main():
             result = query_in_kb(format=format, values=match.groups(), prolog=prolog)
             print_result(format=format, res=result)
         except:
+            print("возникла ошибка")
             continue
 main()
